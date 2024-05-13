ddeaca8a0e3cd6ce8ef92a00d5a3b1e76536d385
+ <+>
2024-05-13 03:59:24 +0300

    cope

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,29 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const handleDeleteClick = () => {
+  
+    if (isDeleting) {
+      return;
+    }
+
+  
+    setIsDeleting(true);
+
+   
+    onDelete(id)
+      .then(() => {
+        
+        setIsDeleting(false);
+      })
+      .catch(() => {
+        
+        setIsDeleting(false);
+      });
+  };
 
   return (
     <li>
@@ -17,7 +40,12 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button
+        onClick={handleDeleteClick}
+        disabled={isDeleting}
+      >
+        {isDeleting ? "Deleting..." : "Delete Question"}
+      </button>
     </li>
   );
 }
